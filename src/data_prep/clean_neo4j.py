4c8666bc59548c478ea3eaec58c684a37aa23f99
Laura Hughes <flaneuse@users.noreply.github.com>
2018-02-02 13:59:20 -0800

    pull/clean data

---
+++ src/data_prep/clean_neo4j.py
@@ -0,0 +1,128 @@
+# @name: clean_neo4j.py
+# @description:  Pythonization of R code to prep neo4j outputs for input into a D3 bar graph/Sankey diagram
+#                R code: https://github.com/flaneuse/ngly1-exploration/blob/master/R/exploration2.Rmd
+# @author: Laura Hughes
+# @email: lhughes@scripps.edu
+# @date: 2 February 2018
+
+# What this function does:
+# 1. imports all .json files within `datain/neo4j`
+# 2. pulls out the pertinent info for the nodes
+# 3. merges annotation ontology terms with the nodes
+# 4. pulls out the edge connections for each specific path number
+# 5. combines nodes and edges into a json file and exports.
+#
+
+
+# setup
+import pandas as pd
+import json
+import os
+from neo4j.v1 import GraphDatabase, basic_auth
+
+datain_dir = 'datain/neo4j/'
+dataout_dir = 'dataout'
+
+# Ideal data structure
+# NODES:
+#   *path_num*     unique id to identify the unique path between the nodes
+#   *node_order*   number corresponding to node order within the path. 0 == source; N == target
+#   *node_id*      unique id for the particular node (e.g. NCBIGene:1071)
+#   *node_name*    name of node (e.g. CETP)
+#   *node_type*    type of node (e.g. GENE)
+#   *ont_terms*    nested object containing ontology term id, name, description, synonyms, iri url, and level in ancestry (0 is root), ontology id
+# EDGES:
+#   *path_num*     unique id to identify the unique path between the nodes
+#   *edge_order*   number corresponding to edge order within the path. 0 == first link from source; N-1 == last link to target   [NOT NEEDED?]
+#   *source_id*    unique id for the source node
+#   *target_id*    unqiue id for the target node
+#   *edge_type*    how terms are related (e.g. 'genetic association')
+#   *edge_url*     url for the source for the edge relationship
+
+# JSON output from neo4j, when returning path:
+# 'data'
+#   [arrayed for each separate path] --> path_num
+#   'row': interleaved nodes and edges, sorted by their order in the path. --> easiest way to get the node_order, edge_order
+#   'meta': metadata abt each node/edge in row.
+#   'graph': unordered list of nodes/edges
+# 'columns' --> name of return obj; in this case, just 'path'
+# 'stats' --> description of paths
+
+# More complicated structure than the output of the neo4j python package, which spits out an enumerator of the structure:
+# - array of data['path']. within that:
+#   - an array of nodes
+#   - an array of edges
+#   ... each with various params/structures, which are all nested objects.
+
+
+# neo4j query interface modified from https://github.com/NuriaQueralt/hypothesis-generation/blob/master/neo4j-hypotheses/q1_1_cypher_to_hypotheses.py
+
+# neo4j query
+query = "MATCH path=(source:GENE)-[:`RO:HOM0000020`]-(:GENE)--(ds:DISO)--(:GENE)-[:`RO:HOM0000020`]-(g1:GENE)--(pw:PHYS)--(target:GENE) WHERE source.id = 'NCBIGene:55768' AND target.id = 'NCBIGene:64772' AND ALL(x IN nodes(path) WHERE single(y IN nodes(path) WHERE y = x)) WITH g1, ds, pw, path, size( (source)-[:`RO:HOM0000020`]-() ) AS source_ortho, size( (g1)-[:`RO:HOM0000020`]-() ) AS other_ortho, max(size( (pw)-[]-() )) AS pwDegree, max(size( (ds)-[]-() )) AS dsDegree, [n IN nodes(path) WHERE n.preflabel IN ['cytoplasm','cytosol','nucleus','metabolism','membrane','protein binding','visible','viable','phenotype']] AS nodes_marked, [r IN relationships(path) WHERE r.property_label IN ['interacts with','in paralogy relationship with','in orthology relationship with','colocalizes with']] AS edges_marked WHERE size(nodes_marked) = 0 AND size(edges_marked) = 0 AND pwDegree < 51 AND dsDegree < 21 RETURN path"
+
+def query_neo4j(query, url = '52.87.232.110', port = '7688', username = "neo4j", pw = "sulabngly1testing"):
+    # initialize neo4j
+    # requires bolt connection to the URI
+    # port must be 7687: first instance of NGLY1 graph
+    # or 7688: second instance of NGLY1 graph (and ports must be open in AWS)
+    driver = GraphDatabase.driver(uri = "bolt://" + url + ":" + port, auth = (username, pw))
+
+    # ask the driver object for a new session & run query
+    # returns an enumerator
+    with driver.session() as session:
+        result = session.run(query)
+
+    # parse query results
+    output = list()
+    path_num = 0
+    for record in result:
+        path_dct = parsePath(record, path_num)
+        output.append(path_dct)
+        path_num += 1
+
+    return output
+        # if (path_num % 10 == 0):
+            # print("Processed " + str(path_num) + "\n")
+
+# Core function to parse neo4j results
+# Pulling out the terms needed to interface with
+def parsePath(path, path_num):
+    # initialize vars
+    out = {}
+    out['nodes'] = []
+    out['edges'] = []
+    n_counter = 0
+    e_counter = 0
+
+    # extract nodes
+    # node objects contain: id, labels, properties(preflabel, description, id)
+    for node in path['path'].nodes:
+        n = {}
+
+        n['path_num'] = path_num
+        n['node_order'] = n_counter
+        n['id'] = node.id # unique id generated by neo4j, used to link to relationships
+        n['node_type'] = list(node.labels)[0]
+        n['node_id'] = node.properties['id'] # unique id for node, used to link to ontologies
+        n['node_name'] = node.properties['preflabel']
+
+        out['nodes'].append(n)
+        n_counter += 1
+    # extract edges
+    # edge objects contain: id, start, end, type, properties(property_description, property_label, reference_uri, reference_date, reference_supporting_text, property_uri)
+    for edge in path['path'].relationships:
+        e = {}
+
+        e['edge_order'] = e_counter
+        e['path_num'] = path_num
+        e['source_id'] = edge.start
+        e['target_id'] = edge.end
+        e['edge_type'] = edge.properties['property_label']
+        e['edge_url'] = edge.properties['reference_uri']
+
+        out['edges'].append(e)
+        e_counter += 1
+    return out
+
+# run the query
+query_neo4j(query)
