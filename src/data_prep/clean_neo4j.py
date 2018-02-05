2a2b1f0604b66bd346e02cc533cb2e39a9665c2a
Laura Hughes <flaneuse@users.noreply.github.com>
2018-02-05 09:48:03 -0800

    switch query output: data[0]['nodes'] --> data['nodes']
    • switch from array of objects to flat dataframes for nodes and edges, arranged in an object
    • added in metapath counting
    • assembled multiple queries

--- src/data_prep/clean_neo4j.py
+++ src/data_prep/clean_neo4j.py
@@ -57,7 +57,16 @@ dataout_dir = 'dataout'
 # neo4j query interface modified from https://github.com/NuriaQueralt/hypothesis-generation/blob/master/neo4j-hypotheses/q1_1_cypher_to_hypotheses.py
 
 # neo4j query
-query = "MATCH path=(source:GENE)-[:`RO:HOM0000020`]-(:GENE)--(ds:DISO)--(:GENE)-[:`RO:HOM0000020`]-(g1:GENE)--(pw:PHYS)--(target:GENE) WHERE source.id = 'NCBIGene:55768' AND target.id = 'NCBIGene:64772' AND ALL(x IN nodes(path) WHERE single(y IN nodes(path) WHERE y = x)) WITH g1, ds, pw, path, size( (source)-[:`RO:HOM0000020`]-() ) AS source_ortho, size( (g1)-[:`RO:HOM0000020`]-() ) AS other_ortho, max(size( (pw)-[]-() )) AS pwDegree, max(size( (ds)-[]-() )) AS dsDegree, [n IN nodes(path) WHERE n.preflabel IN ['cytoplasm','cytosol','nucleus','metabolism','membrane','protein binding','visible','viable','phenotype']] AS nodes_marked, [r IN relationships(path) WHERE r.property_label IN ['interacts with','in paralogy relationship with','in orthology relationship with','colocalizes with']] AS edges_marked WHERE size(nodes_marked) = 0 AND size(edges_marked) = 0 AND pwDegree < 51 AND dsDegree < 21 RETURN path"
+queries = {
+"NGLY1-ENGASE":
+"MATCH path=(source:GENE)-[:`RO:HOM0000020`]-(:GENE)--(ds:DISO)--(:GENE)-[:`RO:HOM0000020`]-(g1:GENE)--(pw:PHYS)--(target:GENE) WHERE source.id = 'NCBIGene:55768' AND target.id = 'NCBIGene:64772' AND ALL(x IN nodes(path) WHERE single(y IN nodes(path) WHERE y = x)) WITH g1, ds, pw, path, size( (source)-[:`RO:HOM0000020`]-() ) AS source_ortho, size( (g1)-[:`RO:HOM0000020`]-() ) AS other_ortho, max(size( (pw)-[]-() )) AS pwDegree, max(size( (ds)-[]-() )) AS dsDegree, [n IN nodes(path) WHERE n.preflabel IN ['cytoplasm','cytosol','nucleus','metabolism','membrane','protein binding','visible','viable','phenotype']] AS nodes_marked, [r IN relationships(path) WHERE r.property_label IN ['interacts with','in paralogy relationship with','in orthology relationship with','colocalizes with']] AS edges_marked WHERE size(nodes_marked) = 0 AND size(edges_marked) = 0 AND pwDegree < 51 AND dsDegree < 21 RETURN path",
+
+"NFE2L1-AQP1":
+"MATCH path=(source:GENE)-[:`RO:HOM0000020`]-(:GENE)--(ds:DISO)--(:GENE)-[:`RO:HOM0000020`]-(g1:GENE)--(pw:PHYS)--(target:GENE) WHERE source.id = 'NCBIGene:4779' AND target.id = 'NCBIGene:358' AND ALL(x IN nodes(path) WHERE single(y IN nodes(path) WHERE y = x)) WITH g1, ds, pw, path, size( (source)-[:`RO:HOM0000020`]-() ) AS source_ortho, size( (g1)-[:`RO:HOM0000020`]-() ) AS other_ortho, max(size( (pw)-[]-() )) AS pwDegree, max(size( (ds)-[]-() )) AS dsDegree, [n IN nodes(path) WHERE n.preflabel IN ['cytoplasm','cytosol','nucleus','metabolism','membrane','protein binding','visible','viable','phenotype']] AS nodes_marked, [r IN relationships(path) WHERE r.property_label IN ['interacts with','in paralogy relationship with','in orthology relationship with','colocalizes with']] AS edges_marked WHERE size(nodes_marked) = 0 AND size(edges_marked) = 0 AND pwDegree < 51 AND dsDegree < 21 RETURN path",
+
+"NGLY1 - AQP1":
+"MATCH path=(source:GENE)-[:`RO:HOM0000020`]-(:GENE)--(ds:DISO)--(:GENE)-[:`RO:HOM0000020`]-(g1:GENE)--(pw:PHYS)--(target:GENE) WHERE source.id = 'NCBIGene:55768' AND target.id = 'NCBIGene:358' AND ALL(x IN nodes(path) WHERE single(y IN nodes(path) WHERE y = x)) WITH g1, ds, pw, path, size( (source)-[:`RO:HOM0000020`]-() ) AS source_ortho, size( (g1)-[:`RO:HOM0000020`]-() ) AS other_ortho, max(size( (pw)-[]-() )) AS pwDegree, max(size( (ds)-[]-() )) AS dsDegree, [n IN nodes(path) WHERE n.preflabel IN ['cytoplasm','cytosol','nucleus','metabolism','membrane','protein binding','visible','viable','phenotype']] AS nodes_marked, [r IN relationships(path) WHERE r.property_label IN ['interacts with','in paralogy relationship with','in orthology relationship with','colocalizes with']] AS edges_marked WHERE size(nodes_marked) = 0 AND size(edges_marked) = 0 AND pwDegree < 51 AND dsDegree < 21 RETURN path"
+}
 
 def query_neo4j(query, url = '52.87.232.110', port = '7688', username = "neo4j", pw = "sulabngly1testing"):
     # initialize neo4j
@@ -72,14 +81,28 @@ def query_neo4j(query, url = '52.87.232.110', port = '7688', username = "neo4j",
         result = session.run(query)
 
     # parse query results
-    output = list()
+    # output = list()
+    nodes = []
+    edges = []
     path_num = 0
+
+# isinstance(, pd.DataFrame)
+# x = path_dct['edges']
+#
+# y = pd.concat([x,x], ignore_index=True)
+# nodes
+# len(y)
     for record in result:
         path_dct = parsePath(record, path_num)
-        output.append(path_dct)
+        if (len(nodes) == 0): # first iteration of loop
+            nodes = path_dct['nodes']
+            edges = path_dct['edges']
+        else:
+            nodes = nodes.append(path_dct['nodes'], ignore_index=True)
+            edges = edges.append(path_dct['edges'], ignore_index=True)
         path_num += 1
 
-    return output
+    return {'nodes': nodes, 'edges': edges}
         # if (path_num % 10 == 0):
             # print("Processed " + str(path_num) + "\n")
 
@@ -87,9 +110,9 @@ def query_neo4j(query, url = '52.87.232.110', port = '7688', username = "neo4j",
 # Pulling out the terms needed to interface with
 def parsePath(path, path_num):
     # initialize vars
-    out = {}
-    out['nodes'] = []
-    out['edges'] = []
+    # out = {}
+    nodes = []
+    edges = []
     n_counter = 0
     e_counter = 0
 
@@ -105,7 +128,7 @@ def parsePath(path, path_num):
         n['node_id'] = node.properties['id'] # unique id for node, used to link to ontologies
         n['node_name'] = node.properties['preflabel']
 
-        out['nodes'].append(n)
+        nodes.append(n)
         n_counter += 1
     # extract edges
     # edge objects contain: id, start, end, type, properties(property_description, property_label, reference_uri, reference_date, reference_supporting_text, property_uri)
@@ -119,9 +142,40 @@ def parsePath(path, path_num):
         e['edge_type'] = edge.properties['property_label']
         e['edge_url'] = edge.properties['reference_uri']
 
-        out['edges'].append(e)
+        edges.append(e)
         e_counter += 1
-    return out
 
-# run the query
-query_neo4j(query)
+    nodes = pd.DataFrame(nodes)
+    edges = pd.DataFrame(edges)
+
+    return {'nodes': nodes, 'edges': edges}
+
+# run the queries
+data = {}
+for key, query in queries.items():
+    print("querying " + key)
+    data[key] = query_neo4j(query)
+
+
+# <<< count_metapaths(data) >>>
+# compress down nodes into a count of metapaths
+# takes a list of nodes and edges and returns a count of node types
+# NOTE: ignores any variation in the verb connecting the terms, e.g. an "is a" relationship versus a "part of" relationship
+def count_metapaths(data):
+    # compress each list of nodes per path down to a metapath: a single string of node_types per path_num
+    # group by path_num and paste/concatenate together the node_types, separated by a hyphen
+    paths = data['nodes'].groupby('path_num')['node_type'].apply(lambda x: '-'.join(x))
+    # group by the metapaths
+    # reset index so there's an additional column containing the path numbers; needed to count the number of paths of each type.
+    return pd.DataFrame(paths).reset_index().groupby('node_type').agg('count')
+
+# Pull out metapaths for all the queries
+metapaths = []
+for key, nodes in data.items():
+    metapath = count_metapaths(nodes)
+    metapath['query'] = key
+
+    if(len(metapaths) == 0):
+        metapaths = metapath
+    else:
+        metapaths = pd.concat([metapath, metapaths])
