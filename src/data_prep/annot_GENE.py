44b10555b1ab5697d2e1e9d3040fccf5453c4af8
Laura Hughes <flaneuse@users.noreply.github.com>
2018-02-08 08:24:54 -0800

    switch to querying mygene.info

--- src/data_prep/annot_GENE.py
+++ src/data_prep/annot_GENE.py
@@ -8,96 +8,153 @@
 # Setup
 import pandas as pd
 import warnings
-base_dir = "datain/annotations/"
+import requests
+import progressbar
+from src.data_prep.clean_neo4j import get_nodes
 
-# -- import function --
-# [1] import files & [2] add in source info ------------------------------------------------
 
-# <<< importGO >>> : reads in ont file from GO, adds in the source info.
-def importGO(file_name, base_dir, n_skip = 0):
+# Pull unique nodes from Nuria's graph
+nodes = get_nodes()
 
-    date_pattern = '[0-9]+/[0-9]+/[0-9][0-9][0-9][0-9]'
+gene_ids = nodes[nodes.node_type == 'GENE']
 
-# Find how many lines to skip; header rows before the real stuff
-    while True:
-        try:
-            ont = pd.read_table(base_dir + file_name, skiprows=n_skip, header=None)
-        except:
-            n_skip += 1
-        else:
-            # header fields: http://geneontology.org/page/go-annotation-file-format-20
-            col_names = ['db', 'db_id', 'db_symbol', 'qualifier',
-            'go_id', 'db_ref', 'evidence_code', 'with_from',
-            'go_root', 'obj_name', 'obj_syn', 'obj_type',
-            'taxon', 'date', 'assigned', 'annot_ext', 'gene_prod_id'
-            ]
+df = trans_result['entrez_dict']
 
-            if ont.shape[1] == len(col_names):
-                # rename columns
-                ont.columns = col_names
+def get_geneterms(gene_ids, transl_url = 'http://mygene.info/v3/query?q=', transl_params = {'entrezonly':'true'}):
+    missing = []
+    annots = pd.DataFrame()
 
-                # TODO: deal with qualifier
-                ont[ont.qualifier != 'NOT']
+    with progressbar.ProgressBar(max_value = max(gene_ids.index)) as bar:
+        for idx, gene in gene_ids.iterrows():
+            if(idx % 50 == 0):
+                print(idx)
 
-                # remove unnecessary columns
-                ont.drop(['db_ref', 'evidence_code', 'obj_type', 'date', 'assigned'], axis=1, inplace=True)
-
+            gene_id = gene['node_id']
+            trans_result = query_translator(gene_id, transl_url = 'http://mygene.info/v3/query?q=', transl_params = {'entrezonly':'true'})
+            if(len(trans_result['missing']) > 0):
+                print("missing = missing.append(trans_result['missing'])")
             else:
-                warnings.warn("unrecognized number of columns; manually rename columns and filter `qualifier` column")
+                gene_dict = trans_result['entrez_dict']
+                annot = query_geneterms(gene_dict)
+                annots = pd.concat([annots, annot])
 
-            break
+            bar.update(idx)
 
-# Pull out the source date and name from the header of the file.
+    return({'annots': annots, 'missing': missing})
+
+
+
+
+def query_translator(gene_id, transl_url = 'http://mygene.info/v3/query?q=', transl_params = {'entrezonly':'true'}, verbose = False):
+    missing = []
+    entrez_dict = {}
+
+    curr_params = transl_params.copy()
+
+    if(gene_id.find('NCBIGene') >= 0):
+        # NCBIGene already set up to do the annotation query, just needs to remove excess in string
+        entrez_dict.update({gene_id: gene_id.replace('NCBIGene:', '')})
+        return({'missing': missing, 'entrez_dict': entrez_dict})
+    elif ((gene_id.find('UniProt') >= 0 )| (gene_id.find('InterPro') >= 0) | (gene_id.find('RGD') >= 0)):
+        # formatting is fine, just needs to be combined
+        gene_query = transl_url + gene_id
+    elif (gene_id.find('FlyBase') >= 0):
+        # formatting is fine, just needs to be combined
+        gene_query = transl_url + gene_id
+        # update species id
+        curr_params.update({'species': 'fruitfly'})
+    elif (gene_id.find('Xenbase') >= 0):
+        # formatting is fine, just needs to be combined
+        gene_query = transl_url + gene_id
+        # update species id
+        curr_params.update({'species': 'frog'})
+    elif (gene_id.find('ZFIN') >= 0):
+        # formatting is fine, just needs to be combined
+        gene_query = transl_url + gene_id
+        # update species id
+        curr_params.update({'species': 'zebrafish'})
+    elif (gene_id.find('WormBase') >= 0):
+        # formatting is fine, just needs to be combined
+        gene_query = transl_url + gene_id
+        # update species id
+        # params.update({'species': 6239}) C. elegans
+        # params.update({'species': 31234}) C. remanei
+    elif (gene_id.find('MGI') >= 0):
+        gene_query = transl_url + 'mgi:' + gene_id.replace('MGI:', 'MGI\\\\:')
+    else:
+        if(verbose):
+            print('unknown gene ' + gene_id)
+        missing.append((gene_id, 'unknown syntax'))
+        return({'missing': missing, 'entrez_dict': entrez_dict})
+
+    # run the request to translate the gene id to an Entrez Gene id
+    transl = requests.get(gene_query, params = curr_params).json()
+
     try:
-        header = pd.read_table(base_dir + file_name, nrows=n_skip -1, header=None)
+        (transl['total'])
     except:
-        # mouse file has a tab-delimited list at the bottom; must skip over.
-        header = pd.read_table(base_dir + file_name, nrows=28, header=None)
+        if(verbose):
+            print('query failed for ' + gene_id)
+        missing.append((gene_id, 'bad query'))
+    else:
+        if(transl['total'] > 1):
+            if(verbose):
+                print('could not uniquely find gene ' + gene_id)
+            missing.append((gene_id, 'not unique'))
+        elif(transl['total'] == 0):
+            if(verbose):
+                print('request ok but could not find ' + gene_id)
+            # print(requests.get(gene_query, params = curr_params).url)
+            missing.append((gene_id, 'not found'))
+        else:
+            try:
+                entrez_dict.update({gene_id: transl['hits'][0]['entrezgene']})
+            except:
+                missing.append((gene_id, '??? no entrez?'))
 
-    source_date = header[0].str.findall(date_pattern)[header[0].str.lower().str.contains('submission date:')]
-    ont['ont_date'] = source_date.to_string(index = False, header = False)
-
-    source = header[0].str.replace('!Project_name: ', '')[header[0].str.contains('Project_name:')]
-
-    ont['ont_source'] = source.to_string(index = False, header = False)
+    return({'missing': missing, 'entrez_dict': entrez_dict})
 
 
-    return ont
 
+def query_geneterms(gene_dict, gene_url = 'https://mygene.info/v3/gene/', gene_params = {'fields':'symbol,name,go'}):
+    if(type(gene_dict) != dict):
+        raise ValueError('gene id is not supplied as a dictionary. Provide a dict with {<merge_id>: <entrez_id>}')
+    GOs = pd.DataFrame()
 
-# import files
-human = importGO('GENE_goa_human.gaf', base_dir)
-rat = importGO('GENE_gene_association.rgd', base_dir)
-mouse = importGO('GENE_gene_association.mgi', base_dir)
-fly = importGO('GENE_gene_association.fb', base_dir)
-zfish = importGO('GENE_gene_association.zfin', base_dir)
-worm = importGO('GENE_gene_association.wb', base_dir)
+    for gene_key, entrez_id in gene_dict.items():
+        result = requests.get(gene_url + str(entrez_id), params = gene_params).json()
 
+        # If GO terms exist, pull them out
+        try:
+            result['go']
+        except:
+            pass
+        else:
+            for key, value in result['go'].items():
+                # if there's a single row, convert it to a list so DataFrame can append properly
+                if (type(value) == dict):
+                    GO = pd.DataFrame([value])
+                else:
+                    GO = pd.DataFrame(value)
+                GOs = pd.concat([GOs, GO], ignore_index=True)
 
-# [3] convert to mergeable IDs -------------------------------------------------
-# To merge properly, unique ids should be in following format:
-# HUMAN: `UniProt:P55087`
-human['id'] = 'UniProt:' + human.db_id.astype(str)
+            GOs['node_id'] = gene_key
+            GOs['entrez_id'] = result['_id']
+            GOs['symbol'] = result['symbol']
+            GOs['gene_name'] = result['name']
 
-# RAT: `RGD:620449`
-rat['id'] = rat.db.astype(str) + ':' + rat.db_id.astype(str)
+        return(GOs)
 
-# MOUSE: `MGI:96646`
-mouse['id'] = mouse.db_id
+# Run the query
+go = get_geneterms(gene_ids)
 
-# FLY: `FlyBase:FBgn0033010`
-fly['id'] = 'FlyBase:' + fly.db_id.astype(str)
+missing = pd.DataFrame(go['missing'], columns = ['gene_id', 'reason']).sort_values(['reason', 'gene_id'])
+missing['db'] = missing.gene_id.apply(lambda x: x[0:3])
 
-# ZEBRAFISH: `ZFIN:ZDB-GENE-040426-2931`
-zfish['id'] = zfish.db.astype(str) + ':' + zfish.db_id.astype(str)
+missing.gene_id
+missing.groupby('reason').db.value_counts()
 
-# WORM: `WormBase:WBGene00013111`
-worm['id'] = worm.db.astype(str) + ':' + worm.db_id.astype(str)
+len(go['missing'])
+
 
 # [4] merge to ontology term names and classify into hierarchial levels (not implemented for now) ------------------------------------------------
-
-
-# [5] append all organisms together ------------------------------------------------
-gene_annot = human.append(rat, ignore_index = True).append(mouse, ignore_index = True).append(fly, ignore_index = True).append(zfish, ignore_index = True).append(worm, ignore_index = True)
-
-gene_annot.keys()
