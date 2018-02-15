4f418c92372659d0465dc7d64b70ae6362716f37
Laura Hughes <flaneuse@users.noreply.github.com>
2018-02-15 10:47:29 -0800

    documentation, transl. node_id to id + ont_id

--- src/data_prep/ont_dict.py
+++ src/data_prep/ont_dict.py
@@ -1,42 +1,101 @@
-# @name: ont_dict.py
-# @title: Creates an ontology dictionary for all terms in the NGLY1 matrix
-# @description:  Main outer file does the merge and checks for missing values.
-#                [Data sources](https://github.com/flaneuse/ntwk-explr/blob/master/datain/DATA_README.md)
-#                [Data pipeline](https://docs.google.com/presentation/d/1dk_1lTGAhB1tJZuUH9yfJoAZwznedHM_DHrCVFBqeW8/edit#slide=id.g3303550b82_0_110)
-# @NOTE: should not be run on ethernet; OLS refuses connection
-# @author: Laura Hughes
-# @email: lhughes@scripps.edu
-# @date: 31 January 2018
+# @name:        ont_dict.py
+# @title:       Creates an ontology dictionary for all terms in the NGLY1 graph network
+# @description: Main outer file does the merge and checks for missing values.
+#               [Data sources](https://github.com/flaneuse/ntwk-explr/blob/master/datain/DATA_README.md)
+#               [Data pipeline](https://docs.google.com/presentation/d/1dk_1lTGAhB1tJZuUH9yfJoAZwznedHM_DHrCVFBqeW8/edit#slide=id.g3303550b82_0_110)
+# @sources:     Ontology structures via OLS (GO, HP, MP, FBcv, FBbt, WormBase); gene annotations via mygene.info; NGLY1 network primarily Monarch
+# @depends:     clean_neo4j.py, annot_GENE.py, ont_struct.py
+# @author:      Laura Hughes
+# @email:       lhughes@scripps.edu
+# @date:        31 January 2018
 
 
 # [0] Setup ----------------------------------------------------------------------
-# output_dir = 'dataout/'  # path within Atom notebook
-output_dir = '../../dataout/' # path from command line prompt
-
 import pandas as pd
 import requests
 import os
 import warnings
 
-# interface to query network
-# from src.data_prep.clean_neo4j import get_nodes  # path within Atom notebook
-from clean_neo4j import get_nodes # path from command line prompt
-
-# functions to pull ontology data
-# import src.data_prep.ont_struct as ont  # path within Atom notebook
-import ont_struct as ont # path from command line prompt
+# -- Atom notebook path settings --
+# output_dir = 'dataout/'  # path within Atom notebook
+# import src.data_prep.clean_neo4j as neo4j  # interface to query network
+# import src.data_prep.annot_GENE as gene # interface to get gene annotations
+# import src.data_prep.ont_struct as ont  # functions to pull ontology data
 
 
-# Pull unique nodes from Nuria's graph
-# node_file = 'https://raw.githubusercontent.com/NuriaQueralt/ngly1/master/neo4j-community-3.0.3/import/ngly1/ngly1_concepts.tsv'
-nodes = get_nodes()
+# -- commpand line prompt settings --
+output_dir = '../../dataout/' # path from command line prompt
+import clean_neo4j as neo4j # interface to query network
+import ont_struct as ont # functions to pull ontology data
 
-nodes.head()
-nodes['type'] = nodes.node_id.apply(lambda x: x[0:2])
-# # -- Pull gene annotations --
-# # genes
-# import src.data_prep.annot_GENE as gene
 
+# [1] Pull unique nodes from Nuria's graph -----------------------------------------------------------------
+# node_file = 'https://raw.githubusercontent.com/NuriaQueralt/ngly1/master/neo4j-community-3.0.3/import/ngly1/ngly1_concepts.tsv' # if want to pull directly from the input file to neo4j
+nodes = neo4j.get_nodes()
+
+# <<< pull_ontsource(id, sep = ':') >>>
+# @name:        pull_ontsource
+# @title:       grab node ID source type
+# @description: used to merge ids to ontology hierarhical levels from OLS; see `check_ontid_unique.py`)
+# @input:       *id* string, *sep*: separator between id source and source-specific id
+# @output:      stub containing the ont source for that particular id
+# @example:     pull_ontsource('ZFIN:ZDB-GENE-051023-7')
+def pull_ontsource(id, sep = ':'):
+    split_id = id.split(sep)
+    if (len(split_id) > 1):
+        return split_id[0]
+    else:
+        warnings.warn("Cannot find the source for the id. Need to change `sep`?")
+
+# (TEST): make sure I'm pulling all the ID types
+# nodes['ont_source'] = nodes.node_id.apply(pull_ontsource)
+# nodes.groupby('node_type').ont_source.value_counts()
+
+# <<< get_ontid(nodes, drop_source = True) >>>
+# @name:        get_ontid
+# @title:       map node ID type to ont_id from OLS
+# @description: used to merge ids to ontology hierarhical levels; see `check_ontid_unique.py`)
+#        NOTE: GENE merging taken care of by merging to annotations (translated via mygene.info) (NCBIGene, ZFIN, MGI, RGD, WormBase, Xenbase, FlyBase, UniProt, InterPro)
+#        NOTE: ignoring PHYS, GENO, VARI for now (PHYS requires too much work for the moment; GENO/VARI are low numbers and would require translating to genes then merging -- if that's even appropriate to lump mutation w/ original function)
+#        Also ignoring, for now:
+#        ANAT      CL (only 7; not in UBERON)
+#        DISO      ZP (271; not in OLS); disease DB: DOID (17), OMIM (6), MESH (4)
+#        TODO: revisit DISO when phenotypes/diseases are better delineated
+#        TODO: revisit GENE when genes/proteins are better delineated
+# @input:       *nodes*: dataframe containing node_id, *drop_source*: binary option to drop column containing the node ont_source
+# @output:      *nodes* dataframe
+# @example:     get_ontid(nodes)
+def get_ontid(nodes, drop_source = True):
+    # ont_source (neo4j graph): ont_id (OLS ID)
+    id2ontid = {
+        # ANAT
+        'UBERON':   'UBERON',
+        # CHEM
+        'CHEBI':    'CHEBI',
+        # DISO
+        'MP': 'mp',
+        'FBbt': 'FBbt',
+        'HP': 'hp',
+        'WBPhenotype': 'wbphenotype',
+        'FBcv': 'FBcv'
+    }
+
+    nodes['ont_source'] = nodes.node_id.apply(pull_ontsource)
+    nodes['ont_id'] = nodes['ont_source'].map(id2ontid)
+
+    if (drop_source):
+        return nodes.drop('ont_source', axis = 1)
+    else:
+        return nodes
+
+nodes = get_ontid(nodes)
+
+# [2] Pull gene annotations -----------------------------------------------------------------
+# 2 purposes:   1) translate node_id (for genes) to standarized NCBI Entrez gene names
+#               2) for each gene, pull associated gene ontology (GO) terms
+
+# [3] Create ontology hierarchical levels for *all* possible terms in base ontologies -----------------------------------------------------------------
+# [4] Merge together nodes in network, annotations, and ontology levels -----------------------------------------------------------------
 # -- Get ontology structures + hierarchy for all ontologies. --
 # OLS ids
 ont_ids = {
@@ -160,7 +219,6 @@ def check_merge(merged):
 onts = create_ont_dict(ont_ids, output_dir)
 
 
-# Create a master template for all the different ontology types
 
 # -- Merge --
 #
