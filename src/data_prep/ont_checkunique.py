e13bb8f6dd9ffd5d0dc2b7594e1607cdb7173a50
Laura Hughes <flaneuse@users.noreply.github.com>
2018-02-15 09:16:05 -0800

    update documentation

--- src/data_prep/ont_checkunique.py
+++ src/data_prep/ont_checkunique.py
@@ -1,9 +1,18 @@
+# @name:        ont_checkunique.py
+# @title:       check if ont IDs are unique (before merge)
+# @description: Ont dictionary terms need to be merged with their term ID populated within the knowledge graph.
+#               However, ontologies don't stick to a single ontology ID; the Mammalian Phenotype ontology, for instance,
+#               uses MP:xxxxx as its core term base, but also incorporates HP:, GO:, etc. terms. So: need to check where
+#               there is overlap between the "unique" ids for each ontology dataset.
+# @author:      Laura Hughes
+# @email:       lhughes@scripps.edu
+# @date:        13 February 2018
+
+# [0] Setup -------------------------------------------------------------------------------------------
 import pandas as pd
 import os
 
 output_dir = 'dataout/' # path within Atom notebook
-
-ont_ids = ['FBcv', 'wbphenotype', 'FBbt', 'UBERON', 'mp', 'hp', 'go', 'CHEBI']
 
 ont_ids = { 'GENE':['go'],
     'DISO': ['FBcv', 'wbphenotype', 'FBbt', 'mp', 'hp'],
@@ -11,6 +20,7 @@ ont_ids = { 'GENE':['go'],
     'CHEM': ['CHEBI']
     }
 
+# [1] Read in files -------------------------------------------------------------------------------------------
 files = sorted(os.listdir(output_dir))
 
 # little helper to see if file has already been generated.
@@ -22,7 +32,7 @@ def check_exists(files, ont_id, file_type):
         return file_name[-1]
     else:
         return False
-# !! TODO: ignore_index / index = id
+
 ont_dicts = pd.DataFrame()
 for ont_type, ont_ids in ont_ids.items():
     print('\n\n---' + ont_type + '---')
@@ -39,13 +49,30 @@ for ont_type, ont_ids in ont_ids.items():
             ont_dict['node_type'] = ont_type
             ont_dicts = pd.concat([ont_dict, ont_dicts], ignore_index=True)
 
-ont_dicts.columns
 
-dupes = ont_dicts.loc[ont_dicts.duplicated(subset=['id', 'node_type']), ['node_type', 'ont_id', 'id']].sort_values('id')
+# [3] Count the duplicates -------------------------------------------------------------------------------------------
+
+# pull the values for the duplicated ids
+# Can't use ID alone; 35,127 dupes
+sum(ont_dicts.duplicated(subset=['id']))
+
+# Potential option: use the node_type (DISO, GENE, PHYS, CHEM, ANAT)
 dupes = ont_dicts.loc[ont_dicts.duplicated(subset=['id']), ['node_type', 'ont_id', 'id']].sort_values('id')
-
 dupes.node_type.value_counts()
+dupes.ont_id.value_counts()
 
+# ID + node_type doesn't work; overlap w/i phenotype (DISO) categories
+# down to 19,108 duplicates
+dupes = ont_dicts.loc[ont_dicts.duplicated(subset=['id', 'node_type']), ['node_type', 'ont_id', 'id']].sort_values('id')
+dupes.shape
+dupes.node_type.value_counts()
+dupes.ont_id.value_counts()
 
+# THE WINNER: ID + ont_id:
+dupes = ont_dicts.loc[ont_dicts.duplicated(subset=['id', 'ont_id']), ['node_type', 'ont_id', 'id']].sort_values('id')
+dupes.shape
 
-ont_dicts[ont_dicts.id == 'UPHENO:0001002']
+# double checking MP is okay (seems to be the weirdo.)
+mp = ont_dicts[ont_dicts.ont_id == 'mp']
+
+sum(mp.duplicated(subset='id'))
