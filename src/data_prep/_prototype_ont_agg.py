7ce2bf3edb62a06e70a557cb2eb5b0d3b39e9122
Laura Hughes <flaneuse@users.noreply.github.com>
2018-02-16 10:46:09 -0800

    initial pull of data

---
+++ src/data_prep/_prototype_ont_agg.py
@@ -0,0 +1,32 @@
+# @name:        _prototype_ont_agg.py
+# @summary:     test out visualizations and aggregations for real prototype NGLY1 pathways
+# @description:
+# @sources:
+# @depends:     ont_dict.py, query_ngly1.py
+# @author:      Laura Hughes
+# @email:       lhughes@scripps.edu
+# @license:     MIT
+# @date:        16 February 2018
+
+# [0: setup]  ----------------------------------------------------------------------------------------------------
+import pandas as pd
+import random
+import src.data_prep.query_ngly1 as ngly1
+
+
+# [1: Pull out all pathways]  -------------------------------------------------------------------------------
+all_paths = ngly1.data
+all_metapaths = ngly1.metapaths
+
+queries = ngly1.queries.keys()
+query = random.sample(queries, 1)[0] # randomly pick a single one
+
+
+paths = all_paths[query]
+
+metapaths = all_metapaths[all_metapaths['query'] == query]
+nodes = paths['nodes']
+edges = paths['edges']
+
+# [2: Test some aggregations] ---------------------------------------------------------------------------
+edges.head()
